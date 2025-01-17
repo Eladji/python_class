fd7f07098981bd820be8c05aeff4498fd44dff75
Eladji <149376126+Eladji@users.noreply.github.com>
2025-01-17 15:18:44 +0100

    working on tp2

--- tp/tp2/class/Pilote.py
+++
@@ -1,7 +0,0 @@
-import Role
-class Pilote(Role):
-    def __init__(self, rep_ship= False, drive_ship = True):
-        super().__init__(rep_ship, drive_ship)
-    
-    def act(self):
-        print("Pilote le vaisseau\n")
--- tp/tp2/class/Technicien.py
+++
@@ -1,5 +0,0 @@
-class Technicien():
-    def __init__(self, rep_ship= True, drive_ship = False):
-        super().__init__(rep_ship, drive_ship)
-    def act(self):
-        print("Répare le vaisseau\n")
