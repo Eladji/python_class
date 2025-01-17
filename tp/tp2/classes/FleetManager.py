4305773feb642afa2a198734d2772837a9956ec0
Eladji <149376126+Eladji@users.noreply.github.com>
2025-01-17 22:00:14 +0100

    polishing

---
+++ tp/tp2/classes/FleetManager.py
@@ -0,0 +1,265 @@
+import json
+from classes.Fleet import Fleet
+from classes.Marchand import Marchand
+from classes.Mentalist import Mentalist
+from classes.Operator import Operator
+from classes.Pilote import Pilote
+from classes.Armurier import Armurier
+from classes.Spaceship import Spaceship
+from classes.Technicien import Technicien
+from classes.Entretien import Entretien
+
+class FleetManager:
+    def __init__(self, json_path='component/default.json'):
+        self.party = self.load_data(json_path)
+        self.current_fleet_index = 0
+    
+    def load_data(self, json_path):
+        with open(json_path, 'r') as file:
+            datas = json.load(file)
+        
+        __metier_map = {
+            "Pilote": Pilote,
+            "Technicien": Technicien,
+            "Armurier": Armurier,
+            "Marchand": Marchand,
+            "Entretien": Entretien
+        }
+        
+        party = []
+        for flotte_data in datas["Flottes"]:
+            fleet = Fleet(flotte_data["Nom"])
+            for vaisseau_data in flotte_data["Vaisseaux"]:
+                ship = Spaceship(vaisseau_data["Nom"], vaisseau_data["Type"], [], vaisseau_data["Etat"])
+                for crew in vaisseau_data["Equipage"]:
+                    if crew["Type"] == "operator":
+                        role_class = __metier_map.get(crew["Metier"])
+                        if role_class:
+                            role_instance = role_class()
+                            operator = Operator(crew["Prenom"], crew["Nom"], crew["Sexe"], crew["Age"], role_instance)
+                            ship.append_member(operator)
+                    elif crew["Type"] == "mentalist":
+                        mentalist = Mentalist(crew["Prenom"], crew["Nom"], crew["Sexe"], crew["Age"], crew["Mana"])
+                        ship.append_member(mentalist)
+                fleet.append_spaceship(ship)
+            party.append(fleet)
+        return party
+    
+    def display_fleet_names(self):
+        for fleet in self.party:
+            print(fleet._name)
+    
+    def change_fleet(self, fleet_name):
+        for index, fleet in enumerate(self.party):
+            if fleet._name == fleet_name:
+                self.current_fleet_index = index
+                return True
+        return False
+    
+    """
+    without those function you need to repeat the same code in each function to find what you need in the party so yes but no
+    """
+    def get_current_fleet(self):
+        return self.party[self.current_fleet_index]
+    
+    def get_fleet_by_name(self):
+        self.display_fleet_names()
+        fleet_name = input("Nom de la flotte: ")
+        for fleet in self.party:
+            if fleet._name == fleet_name:
+                return fleet
+        return None
+    
+    def get_ship(self,current_fleet = None):
+        if current_fleet == None:
+            fleet = self.get_fleet_by_name
+            input_ship = input("Nom du vaisseau: ")
+            for ship in fleet._spaceship:
+                if ship._name == input_ship:
+                    return ship
+                else:
+                    return None
+        elif current_fleet:
+            self.get_current_fleet().display_spaceships()
+            input_ship = input("Nom du vaisseau: ")
+            for ship in current_fleet._spaceship:
+                if ship._name == input_ship:
+                    return ship
+        else:
+            return None
+                
+    def get_member(self, ship):
+        ship.display_crew()
+        input_member = input("Nom du membre: ")
+        for member in self.get_ship()._crew:
+            if member._first_name == input_member:
+                return member
+            else:
+                return None
+    
+    
+    def show_menu(self):
+        print("\n\t\tMenu\t\n")
+        print(f"\tFlotte actuelle:\n\t {self.get_current_fleet()._name}\n")
+        print("0 - afficher les flottes")
+        print("1 - ajouter une flotte")
+        print("2 - suppression d'une flotte")
+        print("3 - renommage d'une flotte")
+        print("4 - Supprimer un membre de l'équipage")
+        print("5 - Changement de flotte")
+        print("6 - Ajouter un membre à l'équipage")
+        print("7 - vérifier la préparation d'un vaisseau")
+        print("8 - afficher l'équipage d'un vaisseau")
+        print("9 - ajouter un vaisseau")
+        print("10 - afficher les vaisseaux d'une flotte")
+        print("11 - Supprimer un vaisseau")
+        print("6 - Quitter")
+        return input("Votre choix: ")
+
+    def input_handler(self):
+        choice = self.show_menu()
+        match choice:
+            case "0":
+                self.display_fleet_names()
+            case "1":
+                self.add_fleet()
+            case "2":
+                fleet_name = input("Nom de la flotte: ")
+                for fleet in self.party:
+                    if fleet._name == fleet_name:
+                        self.party.remove(fleet)
+            case "3":
+                self.rename_fleet()
+            case "4":
+                self.remove_member_from_ship()
+            case "5":
+                self.change_fleet_by_name()
+            case "6":
+                self.add_member_to_ship()
+            case "7":
+                check = self.get_current_fleet().check_preparation()
+                if check:
+                    print("Le vaisseau est prêt")
+                else:
+                    print("Le vaisseau n'est pas prêt")
+            case "8":
+                self.display_fleet_names()
+                fleet_name = input("Nom de la flotte: ")
+                for fleet in self.party:
+                    if fleet._name == fleet_name:
+                        for ships in fleet._spaceship:
+                            print(ships._name)
+                    ship_name = input("Nom du vaisseau: ")
+                    for ship in fleet._spaceship:
+                        if ship._name == ship_name:
+                            ship.display_crew()
+            case "9":
+                self.add_ship_to_fleet()
+            case "10":
+                self.display_fleet_names()
+                fleet_name = input("Nom de la flotte: ")
+                for fleet in self.party:
+                    if fleet._name == fleet_name:
+                        fleet.display_spaceships()
+    
+    def rename_fleet(self):
+        fleet_name = input("Nom de la flotte: ")
+        new_name = input("Nouveau nom: ")
+        if new_name == "" or len(new_name) <= 0 or len(new_name) > 20:
+            print("Nom invalide")
+            return
+        for fleet in self.party:
+            if fleet._name == fleet_name:
+                fleet._name(new_name)
+                
+    def add_fleet(self):
+        fleet_name = input("Nom de la flotte: ")
+        self.party.append(Fleet(fleet_name))
+        
+    def add_ship_to_fleet(self):
+        add_ship = input("Voulez-vous ajouter un vaisseau à une flotte existante ? (Oui, Non): ")
+        if add_ship == "Non":
+            self.add_fleet()
+        self.display_fleet_names()
+        fleet_name = input("Nom de la flotte: ")
+        for fleet in self.party:
+            if fleet._name == fleet_name:
+                ship_name = input("Nom du vaisseau: ")
+                if ship_name == "" or len(ship_name) <= 0 or len(ship_name) > 20:
+                    print("Nom invalide")
+                    return
+                ship_type = input("Type de vaisseau: ")
+                if ship_type != "Transport" or ship_type != "Guerre":
+                    return
+                fleet.append_spaceship(Spaceship(ship_name, ship_type))
+                
+    def add_member_to_ship(self):
+        fleet = self.get_current_fleet()
+        fleet.display_spaceships()
+        ship_name = input("Nom du vaisseau: ")
+        for ship in fleet._spaceship:
+            if ship._name == ship_name:
+                input_last_name = input("Nom du membre: ")
+                if input_last_name == "" or len(input_last_name) <= 0 or len(input_last_name) > 20:
+                    print("Nom invalide")
+                    return
+                input_first_name = input("Prénom du membre: ")
+                if input_first_name == "" or len(input_first_name) <= 0 or len(input_first_name) > 20:
+                    print("Prénom invalide")
+                    return
+                input_age = int(input("Age du membre: "))
+                if input_age <= 0 or input_age > 100:
+                    print("Age invalide")
+                    return
+                input_gender = input("Sexe du membre (Homme, Femme): ")
+                if input_gender != "Homme" or "Femme":
+                    print("Sexe invalide")
+                    return
+                input_Type = input("Type de membre (Operator, Mentalist): ")
+                if input_Type == "Operator":
+                    input_role = input("Role du membre (Pilote, Technicien, Armurier, Marchand, Entretien): ")
+                    if input_role != "Pilote" or input_role != "Technicien" or input_role != "Armurier" or input_role != "Marchand" or input_role != "Entretien":
+                        print("Role invalide")
+                        return
+                    else:
+                        role_class = self.__metier_map.get(input_role)
+                        if role_class:
+                            role_instance = role_class()
+                    new_member = Operator(input_first_name, input_last_name, input_gender, input_age, role_instance)
+                if input_Type == "Mentalist":
+                    new_member = Mentalist(input_first_name, input_last_name, input_gender, input_age)
+                elif input_Type != "Mentalist" or input_Type != "Operator":
+                    print("Type de membre invalide")
+                    return
+                ship.append_member(new_member)
+                        
+    def remove_member_from_ship(self):
+        fleet = self.get_current_fleet()
+        fleet.display_spaceships()
+        ship_name = input("Nom du vaisseau: ")
+        for ship in fleet._spaceship:
+            if ship._name == ship_name:
+                ship_name = input("Nom du vaisseau: ")
+                for ship in fleet._spaceship:
+                    if ship._name == ship_name:
+                        ship.display_crew()
+                        member_name = input("Nom du membre: ")
+                        for member in ship._crew:
+                            if member._first_name == member_name:
+                                ship.remove_member(member)
+
+    def change_fleet_by_name(self):
+        self.display_fleet_names()
+        fleet_name = input("Nom de la flotte:\n")
+        if not self.change_fleet(fleet_name):
+            print("Flotte inexistante")
+
+    def run(self):
+        while True:
+            self.input_handler()
+
+
+# Instantiate FleetManager and run
+if __name__ == "__main__":
+    fleet_manager = FleetManager()
+    fleet_manager.run()
